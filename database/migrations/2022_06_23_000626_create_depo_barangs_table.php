48da84e7ee4bbabdd3b166512b9a8e54178ef395
bernadlein <leinsgreenadz@gmail.com>
2022-07-28 22:03:04 +0700

    Migration

--- database/migrations/2022_06_23_000626_create_depo_barangs_table.php
+++ database/migrations/2022_06_23_000626_create_depo_barangs_table.php
@@ -21,6 +21,7 @@ return new class extends Migration {
                 ->cascadeOnDelete();
             $table->integer('stok')
                 ->default(0);
+            $table->softDeletes();
         });
     }
 
